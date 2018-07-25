cb5aaba3981b045ff3fa98998f12a98a51a6831c
gracehillen <grahillen19@ahastars.org>
2018-07-25 19:34:44 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,11 +1,11 @@
 
 def create_olympics_hash
-  summer_olympics = {Sydney: 2000, Athens: 2004, Beijing: 2008, London: 2012}
+  summer_olympics = {Sydney: "2000", Athens: "2004", Beijing: "2008", London: "2012"}
 end
 puts create_olympics_hash
 
 def add_a_key_value_pair
-  summer_olympics[:Atlanta] = 1996
+  summer_olympics[:Atlanta] = "1996"
 end
 puts add_a_key_value_pair
 
