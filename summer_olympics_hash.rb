ac67871d3b7b0466230ba70d1bf585cf6429f420
gracehillen <grahillen19@ahastars.org>
2018-07-25 19:36:52 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -11,9 +11,7 @@ puts add_a_key_value_pair
 
 def iterate_through_hash
   summer_olympics.each do |x,y|
-  puts "The price of #{x} is $#{y}."
-  # Implement this method so that it iterates over the hash created in add_a_key_value_pair
-  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
+  puts "The #{x} summer olympics took place in #{y}"
 end
 
 def iterate_through_keys
