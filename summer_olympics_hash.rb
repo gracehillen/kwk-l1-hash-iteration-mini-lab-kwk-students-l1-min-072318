c4fdbd4903545edcc1e25e9121eea870ec847750
gracehillen <grahillen19@ahastars.org>
2018-07-25 19:26:16 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,6 +1,7 @@
 
 def create_olympics_hash
-  localyear = {"Sydney" => 2000, 
+  localyear = {"Sydney" => 2000, "Athens" => 2004, "Beijing" => 2008, "London" => 2012}
+  
 end
 
 def add_a_key_value_pair
